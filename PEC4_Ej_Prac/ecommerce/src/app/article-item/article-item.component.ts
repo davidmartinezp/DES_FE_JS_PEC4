bad17f70933046a2f99c2f05a3c34cd34310b888
davidmartinezp <davidmartinezp@uoc.edu>
2023-12-09 20:50:09 +0100

    finalizado

--- PEC4_Ej_Prac/ecommerce/src/app/article-item/article-item.component.ts
+++ PEC4_Ej_Prac/ecommerce/src/app/article-item/article-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { Article } from '../models/article.model';
 import { ArticleQuantityChange } from '../models/article-quantity-change.model';
 
@@ -6,37 +6,45 @@ import { ArticleQuantityChange } from '../models/article-quantity-change.model';
 @Component({
   selector: 'app-article-item',
   templateUrl: './article-item.component.html',
-  styleUrl: './article-item.component.css'
+  styleUrl: './article-item.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ArticleItemComponent {
   @Input() article?: Article;
-  @Output() articleQuantityChange: EventEmitter<ArticleQuantityChange> = new EventEmitter();
-  @Output() increment = new EventEmitter<void>();
-  @Output() decrement = new EventEmitter<void>();
+  // @Output() articleQuantityChange: EventEmitter<ArticleQuantityChange> = new EventEmitter();
+  @Output() increment = new EventEmitter<number>();
+  @Output() decrement = new EventEmitter<number>();
 
   incrementQuantity() {
     if (this.article) {
-      this.article.quantityInCart++;
-      this.emitArticleQuantityChange();
+      this.increment.emit(this.article.id);
     }
+    
+    // if (this.article) {
+    //   this.article.quantityInCart++;
+    //   this.emitArticleQuantityChange();
+    // }
   }
   
   decrementQuantity() {
-    this.increment.emit();
-    if (this.article && this.article.quantityInCart > 0) {
-      this.article.quantityInCart--;
-      this.emitArticleQuantityChange();
+    if (this.article) {
+      this.decrement.emit(this.article.id);
     }
+    
+    // if (this.article && this.article.quantityInCart > 0) {
+    //   this.article.quantityInCart--;
+    //   this.emitArticleQuantityChange();
+    // }
   }
 
-  private emitArticleQuantityChange() {
-    if (this.article) {
-      const articleQuantityChange: ArticleQuantityChange = {
-        article: this.article,
-        quantity: this.article.quantityInCart
-      };
+  // private emitArticleQuantityChange() {
+  //   if (this.article) {
+  //     const articleQuantityChange: ArticleQuantityChange = {
+  //       article: this.article,
+  //       quantity: this.article.quantityInCart
+  //     };
   
-      this.articleQuantityChange.emit(articleQuantityChange);
-    }
-  }
+  //     this.articleQuantityChange.emit(articleQuantityChange);
+  //   }
+  // }
 }
--- PEC4_Ej_Prac/ecommerce/src/app/article-list/article-list.component.ts
+++ PEC4_Ej_Prac/ecommerce/src/app/article-list/article-list.component.ts
@@ -2,14 +2,39 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-article-list',
-  template: `<div *ngFor="let article of articles">
-                <app-article-item [article]="article"></app-article-item>
+  template: `
+            <div class="article-list-container">
+              <div *ngFor="let article of articles">
+                  <app-article-item [article]="article" (increment)="incrementQuantity($event)" (decrement)="decrementQuantity($event)"></app-article-item>
+              </div>
              </div>`,
-  styles: ``
+  styles: `
+  .article-list-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center; 
+  }
+    `
 })
 export class ArticleListComponent {
+
+  incrementQuantity(articleId: number) {
+    const article = this.articles.find(a => a.id === articleId);
+    if (article) {
+      article.quantityInCart++;
+    }
+  }
+
+  decrementQuantity(articleId: number) {
+    const article = this.articles.find(a => a.id === articleId);
+    if (article && article.quantityInCart > 0) {
+      article.quantityInCart--;
+    }
+  }
+
   articles = [
     {
+      id: 1,
       name: 'JavaScript y Angular: De los fundamentos del lenguaje al desarrollo de una aplicación web',
       imageUrl: '../assets/JSYAngular.jpg',
       price: 28.41,
@@ -17,6 +42,7 @@ export class ArticleListComponent {
       quantityInCart: 0
     },
     {
+      id: 2,
       name: 'AngularJS',
       imageUrl: '../assets/AngularJS.jpg',
       price: 36.10,
@@ -24,6 +50,7 @@ export class ArticleListComponent {
       quantityInCart: 0
     },
     {
+      id: 3,
       name: 'El gran libro de Angular',
       imageUrl: '../assets/AngularGranLibro.jpg',
       price: 23.56,
